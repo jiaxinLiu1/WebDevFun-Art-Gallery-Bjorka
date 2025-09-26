a0049ab0a6497b5a8b0e45aefe452515cecc62eb
Yuhong03 <jiyu24ln@student.ju.se>
2025-09-26 12:06:41 +0200

    add handlebars and modified js file

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const port = 8080;
 const app = express();
 // MIDDLEWARES
 app.use(express.static("public"));
-// ROUTES
-app.get("/", (req, res) => res.sendFile(__dirname + "/views/main.html"));
 // LISTEN TO INCOMING REQUESTS
 app.listen(port, () =>
   console.log(`Server up and running on http://localhost:${port}...`)
 );
+// LOAD THE HANDLEBARS PACKAGE FOR EXPRESS
+const { engine }=require("express-handlebars");
+// HANDLEBARS SETTINGS
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+// ROUTE TO RENDER A HANDLEBARS PAGE
+app.get("/", (req, res) => {
+  res.render('contact')
+});
