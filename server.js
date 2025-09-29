c93f4ab6674e7512fa8e57c0cdb7142e19b3cb51
Yuhong03 <jiyu24ln@student.ju.se>
2025-09-29 19:39:40 +0200

    added handlebars and installed

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 // PACKAGES
 const express = require("express");
+const path = require("path");
+const { engine } = require("express-handlebars");
+
 // VARIABLES AND CONSTANTS
 const port = 8080;
 const app = express();
+
 // MIDDLEWARES
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname)); 
+
+// HANDLEBARS SETTINGS
+app.engine("handlebars", engine({
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views", "layouts"),
+}));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.render("index", { title: "Björka" });
+});
+
 // LISTEN TO INCOMING REQUESTS
 app.listen(port, () =>
   console.log(`Server up and running on http://localhost:${port}...`)
 );
-// LOAD THE HANDLEBARS PACKAGE FOR EXPRESS
-const { engine }=require("express-handlebars");
-// HANDLEBARS SETTINGS
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
-// ROUTE TO RENDER A HANDLEBARS PAGE
-app.get("/", (req, res) => {
-  res.render('contact')
-});
