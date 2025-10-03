6cdea21f254665351d6276c0449f31bc75808ae4
jiaxinLiu1 <liji23zn@student.ju.se>
2025-10-03 20:53:29 +0200

    11

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 const express = require("express");
 const path = require("path");
 const {engine} = require("express-handlebars");
+const sqlite3 = require("sqlite3");
 
 // VARIABLES AND CONSTANTS
 const port = 8080;
 const app = express();
+const dbFile = "exhibition-data1.sqlite3.db";
+db = new sqlite3.Database(dbFile);
 // JSONvariable
 const exhibitions = [
   {
+    id: "1",
     name: "Impressionist Art",
     description: "Collection of impressionist paintings",
   },
-  {
-    name: "Modern Sculpture",
-    description: "Contemporary sculptures",
-  },
+  {id: "2", name: "Modern Sculpture", description: "Contemporary sculptures"},
 ];
 // MIDDLEWARES
 app.use(express.static(path.join(__dirname, "public")));
@@ -53,6 +54,36 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
+
+//create the "exhibition"table to the database
+db.run(
+  `CREATE TABLE skills (
+     sid INTEGER PRIMARY KEY AUTOINCREMENT,
+     sname TEXT NOT NULL,
+     sdesc TEXT NOT NULL,
+     stype TEXT NOT NULL,
+     slevel INT
+  )`,
+  (error) => {
+    if (error) {
+      console.log("ERROR: ", error);
+    } else {
+      console.log("Exhibitions table created successfully!");
+      //inserts exhibitions
+      exhibitions.forEach((oneExhibition) => {
+        db.run`INSERT INTO exhibitions (name, description) VALUES (?, ?)`,
+          [oneExhibition.name, oneExhibition.description],
+          (err) => {
+            if (err) {
+              console.log("ERROR inserting exhibition: ", err);
+            } else {
+              console.log(`Inserted exhibition: ${oneExhibition.name}`);
+            }
+          };
+      });
+    }
+  }
+);
 
 // LISTEN TO INCOMING REQUESTS
 app.listen(port, () =>
